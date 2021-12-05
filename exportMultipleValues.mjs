3ffc0dd3f0cf1f085fcd2d5fcb6dbdd886b3cf70
Divy260 <divya.matavalam828@gmail.com>
2021-12-05 05:38:12 +0000

    export multiple values using es6 solution

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,9 @@
+const myArray = ["camel", 265, true, "5.6"];
+const bulb = {
+  watts: 10,
+  type: "LED",
+};
+function multiplyByFour(nbr) {
+  return nbr * 4;
+}
+export { myArray, bulb, multiplyByFour };
